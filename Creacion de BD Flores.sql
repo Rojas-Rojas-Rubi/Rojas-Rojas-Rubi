2432d532ee0da3e94864034896231fdb837fa5ef
Rojas-Rojas-Rubi <patitomio29@gmail.com>
2025-05-28 12:00:42 -0600

    Creacion de BD Flores.sql
    script de la base de datos

--- Creacion de BD Flores.sql
+++ Creacion de BD Flores.sql
@@ -1,23 +1,12 @@
 CREACION DE LA BASE DE DATOS
 
-CREATE DATABASE pueblos_magicos;
 CREATE DATABASE florería;
 
 USE florería;
 
-CREATE TABLE flor (
-    id_flor INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50),
-    color VARCHAR(30),
-    precio DECIMAL(6,2)
+CREATE TABLE flores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    color VARCHAR(50),
+    cantidad INT
 );
-
-CREATE TABLE inventario (
-    id_inventario INT AUTO_INCREMENT PRIMARY KEY,
-    cantidad INT,
-    proveedor VARCHAR(50),
-    id_flor INT UNIQUE,
-    FOREIGN KEY (id_flor) REFERENCES flor(id_flor)
-);
-
-
